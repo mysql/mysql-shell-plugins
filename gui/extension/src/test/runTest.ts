74b76e4a17bc2cb0d7970d505d88cfbe46192057
Mike Zinner <mike.zinner@oracle.com>
2022-04-28 20:10:34 +0200

    Fixes for extension tests
    Change-Id: I956513522e4f7dfc90b1a3b1c8a123655c22e87f

--- gui/extension/src/test/runTest.ts
+++ gui/extension/src/test/runTest.ts
@@ -29,18 +29,23 @@ export const main = async (): Promise<void> => {
     try {
         // The folder containing the Extension Manifest package.json
         // Passed to `--extensionDevelopmentPath`
-        const extensionDevelopmentPath = path.resolve(__dirname, "../../");
+        const extensionDevelopmentPath = path.resolve(__dirname, "../../../../");
 
         // The path to the extension test script
         // Passed to --extensionTestsPath
         const extensionTestsPath = path.resolve(__dirname, "./suite/index");
 
         // Download VS Code, unzip it and run the integration test
-        await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs: ["--disable-extensions"] });
+        await runTests({
+            extensionDevelopmentPath,
+            extensionTestsPath,
+            launchArgs: ["--disable-extensions"],
+            extensionTestsEnv: { extensionTestsRunning: "running" } });
     } catch (err) {
         console.error("Failed to run tests");
         process.exit(1);
     }
+    process.exit(0);
 };
 
 (async () => {
