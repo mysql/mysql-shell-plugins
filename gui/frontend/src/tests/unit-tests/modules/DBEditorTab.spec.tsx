7a6cb6d4538d5b1d45858e7f4b00fadd918bf545
Marcin Marek <marcin.marek@oracle.com>
2022-05-05 14:22:00 +0200

    new unit tests
    - tests for DBEditor
    - tests for shell modules
    - tests for other modules
    
    Change-Id: Ia563dc10def436268a0402b11d1189d1b9cc7d12

---
+++ gui/frontend/src/tests/unit-tests/modules/DBEditorTab.spec.tsx
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2022, Oracle and/or its affiliates.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2.0,
+ * as published by the Free Software Foundation.
+ *
+ * This program is also distributed with certain software (including
+ * but not limited to OpenSSL) that is licensed under separate terms, as
+ * designated in a particular file or component or in included license
+ * documentation.  The authors of MySQL hereby grant you an additional
+ * permission to link the program and your derivative works with the
+ * separately licensed software that they have included with MySQL.
+ * This program is distributed in the hope that it will be useful,  but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ * the GNU General Public License, version 2.0, for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+import { mount } from "enzyme";
+import React from "react";
+import { setupShellForTests } from "../test-helpers";
+import { DBEditorTab, IDBEditorTabPersistentState, IOpenEditorState } from "../../../modules/scripting/DBEditorTab";
+import { ShellInterfaceSqlEditor } from "../../../supplement/ShellInterface/ShellInterfaceSqlEditor";
+import { ExecutionWorkerPool } from "../../../modules/scripting/execution/ExecutionWorkerPool";
+import { DBType } from "../../../supplement/ShellInterface";
+import { CodeEditorMode, ICodeEditorModel, IEditorPersistentState } from "../../../components/ui/CodeEditor/CodeEditor";
+import { EntityType } from "../../../modules/scripting";
+import { ExecutionContexts } from "../../../script-execution/ExecutionContexts";
+import { Monaco } from "../../../components/ui/CodeEditor";
+import { MySQLShellLauncher } from "../../../utilities/MySQLShellLauncher";
+jest.mock("../../../components/ui/CodeEditor/CodeEditor");
+
+describe("DBEditor module tests", (): void => {
+
+    let content = `\nprint("typescript");\n\\js\n`;
+    content += `\nprint("javascript");\n\\sql\n`;
+    content += `\nselect "(my)sql" from dual;\n\\py\n`;
+    content += `\nprint("python");\n`;
+    const model = Monaco.createModel("", "msg") as ICodeEditorModel;
+    model.executionContexts = new ExecutionContexts(undefined, 80024, "", "");
+    model.editorMode = CodeEditorMode.Standard;
+    model.setValue(content);
+
+    let launcher: MySQLShellLauncher;
+
+    beforeAll(async () => {
+        launcher = await setupShellForTests("DBEditor", false, true, "DEBUG2");
+    });
+
+    afterAll(async () => {
+        await launcher.exitProcess();
+    });
+
+    it("Test DBEditorTab instantiation", () => {
+        const wp = new ExecutionWorkerPool();
+        const eps: IEditorPersistentState = {
+            viewState: null,
+            model,
+            options: {},
+        };
+        const edState: IOpenEditorState = {
+            state: eps,
+            currentVersion: 0,
+            type: EntityType.Console,
+            id: "SQLEditor",
+            caption: "",
+        };
+        const backend = new ShellInterfaceSqlEditor();
+        const savedState: IDBEditorTabPersistentState = {
+            backend,
+            serverVersion: 1,
+            serverEdition: "gpl",
+            sqlMode: "mode1",
+            editors: [edState],
+            scripts: [],
+            schemaTree: [],
+            explorerState: new Map ([["state1", {}]]),
+            activeEditor: "SQLEditor",
+            currentSchema: "myDb",
+            explorerWidth: 200,
+        };
+        const component = mount<DBEditorTab>(
+            <DBEditorTab
+                connectionId={0}
+                dbType={DBType.MySQL}
+                savedState={savedState}
+                workerPool={wp}
+                showAbout={false}
+            />,
+        );
+        const props = component.props();
+        expect(props.connectionId).toEqual(0);
+        expect(props.dbType).toEqual(DBType.MySQL);
+        expect(props.savedState).toEqual(savedState);
+        expect(props.workerPool).toEqual(wp);
+        component.unmount();
+    });
+
+});
---
+++ gui/frontend/src/tests/unit-tests/modules/StandalonePresentationInterface.spec.tsx
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2022, Oracle and/or its affiliates.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2.0,
+ * as published by the Free Software Foundation.
+ *
+ * This program is also distributed with certain software (including
+ * but not limited to OpenSSL) that is licensed under separate terms, as
+ * designated in a particular file or component or in included license
+ * documentation.  The authors of MySQL hereby grant you an additional
+ * permission to link the program and your derivative works with the
+ * separately licensed software that they have included with MySQL.
+ * This program is distributed in the hope that it will be useful,  but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ * the GNU General Public License, version 2.0, for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+import { mount } from "enzyme";
+import React from "react";
+import { StandaloneScriptEditor } from "../../../modules/scripting/StandaloneScriptEditor";
+import { Monaco } from "../../../components/ui/CodeEditor";
+import {
+    CodeEditor, CodeEditorMode, ICodeEditorModel, IEditorPersistentState,
+} from "../../../components/ui/CodeEditor/CodeEditor";
+import { ExecutionContexts } from "../../../script-execution/ExecutionContexts";
+import { StandalonePresentationInterface } from "../../../modules/scripting/execution/StandalonePresentationInterface";
+
+
+describe("Standalone presentation interface tests", (): void => {
+
+    let content = `\nprint("typescript");\n\\js\n`;
+    content += `\nprint("javascript");\n\\sql\n`;
+    content += `\nselect "(my)sql" from dual;\n\\py\n`;
+    content += `\nprint("python");\n`;
+    const model = Monaco.createModel("", "msg") as ICodeEditorModel;
+    model.executionContexts = new ExecutionContexts(undefined, 80024, "", "");
+    model.editorMode = CodeEditorMode.Standard;
+    model.setValue(content);
+
+    it("Standalone presentation interface instantiation", () => {
+        const innerRef = React.createRef<HTMLDivElement>();
+        const eps: IEditorPersistentState = {
+            viewState: null,
+            model,
+            options: {},
+        };
+        const component = mount<StandaloneScriptEditor>(
+            <StandaloneScriptEditor
+                editorState={eps}
+            />,
+        );
+        const spi = new StandalonePresentationInterface(
+            new StandaloneScriptEditor({editorState: eps}),
+            new CodeEditor({ allowSoftWrap: true}),
+            "sql",
+            innerRef,
+        );
+        expect(spi.language).toEqual("sql");
+        component.unmount();
+    });
+});
