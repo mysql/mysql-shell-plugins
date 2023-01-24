6d1b3180c7103433c79f6502a29c99ab04361eff
Mike Zinner <mike.zinner@oracle.com>
2023-01-24 21:50:45 +0100

    MRS Metadata Schema fix
    Change-Id: If69eb33fbd066fecfdbf0c6ca24b4673d5dd078d

--- mrs_plugin/db_schema/mrs_metadata_schema_1.0.30_to_1.0.31.sql
+++ mrs_plugin/db_schema/mrs_metadata_schema_1.0.30_to_1.0.31.sql
@@ -6,6 +6,9 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 
 USE `mysql_rest_service_metadata`;
 
+ALTER TABLE `mysql_rest_service_metadata`.`router`
+	MODIFY COLUMN `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'The ID of the router instance and is a unique identifier of the server instance.';
+
 DROP TRIGGER `mysql_rest_service_metadata`.`mrs_user_BEFORE_INSERT`;
 DROP TRIGGER `mysql_rest_service_metadata`.`mrs_user_BEFORE_UPDATE`;
 
@@ -14,14 +17,19 @@ DELIMITER $$
 CREATE DEFINER = CURRENT_USER TRIGGER `mysql_rest_service_metadata`.`mrs_user_BEFORE_INSERT` BEFORE INSERT ON `mrs_user` FOR EACH ROW
 BEGIN
 	IF NEW.name IS NOT NULL AND (SELECT COUNT(*) FROM `mysql_rest_service_metadata`.`mrs_user` AS u 
-		WHERE UPPER(u.name) = UPPER(NEW.name) AND u.auth_app_id = NEW.auth_app_id) > 0
+		WHERE UPPER(u.name) = UPPER(NEW.name) AND u.auth_app_id = NEW.auth_app_id AND NEW.id <> u.id) > 0
 	THEN
 		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "This name has already been used.";
 	END IF;
 	IF NEW.email IS NOT NULL AND (SELECT COUNT(*) FROM `mysql_rest_service_metadata`.`mrs_user` AS u 
-		WHERE UPPER(u.email) = UPPER(NEW.email) AND u.auth_app_id = NEW.auth_app_id) > 0
+		WHERE UPPER(u.email) = UPPER(NEW.email) AND u.auth_app_id = NEW.auth_app_id AND NEW.id <> u.id) > 0
 	THEN
 		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "This email has already been used.";
+    END IF;
+    IF (NEW.auth_string IS NULL AND 
+        (SELECT a.auth_vendor_id FROM `mysql_rest_service_metadata`.`auth_app` AS a WHERE a.id = NEW.auth_app_id) = 0x30000000000000000000000000000000)
+    THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "A this account requires a password to be set.";
     END IF;
     IF JSON_STORAGE_SIZE(NEW.app_options) > 16384 THEN
 		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "The JSON value stored in app_options must not be bigger than 16KB.";
@@ -31,14 +39,19 @@ END$$
 CREATE DEFINER = CURRENT_USER TRIGGER `mysql_rest_service_metadata`.`mrs_user_BEFORE_UPDATE` BEFORE UPDATE ON `mrs_user` FOR EACH ROW
 BEGIN
 	IF NEW.name IS NOT NULL AND (SELECT COUNT(*) FROM `mysql_rest_service_metadata`.`mrs_user` AS u 
-		WHERE UPPER(u.name) = UPPER(NEW.name) AND u.auth_app_id = NEW.auth_app_id) > 0
+		WHERE UPPER(u.name) = UPPER(NEW.name) AND u.auth_app_id = NEW.auth_app_id AND NEW.id <> u.id) > 0
 	THEN
 		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "This name has already been used.";
 	END IF;
 	IF NEW.email IS NOT NULL AND (SELECT COUNT(*) FROM `mysql_rest_service_metadata`.`mrs_user` AS u 
-		WHERE UPPER(u.email) = UPPER(NEW.email) AND u.auth_app_id = NEW.auth_app_id) > 0
+		WHERE UPPER(u.email) = UPPER(NEW.email) AND u.auth_app_id = NEW.auth_app_id AND NEW.id <> u.id) > 0
 	THEN
 		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "This email has already been used.";
+    END IF;
+    IF (NEW.auth_string IS NULL AND 
+        (SELECT a.auth_vendor_id FROM `mysql_rest_service_metadata`.`auth_app` AS a WHERE a.id = NEW.auth_app_id) = 0x30000000000000000000000000000000)
+    THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "A this account requires a password to be set.";
     END IF;
     IF JSON_STORAGE_SIZE(NEW.app_options) > 16384 THEN
 		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "The JSON value stored in app_options must not be bigger than 16KB.";
