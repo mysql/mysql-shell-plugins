0ef7c4dce0b9a407da323315707092cf5d8b9d22
Miguel Tadeu <miguel.tadeu@oracle.com>
2023-03-13 15:35:11 +0000

    Bug#35135302 - MRS - Custom URL for Access Token field is not saved
    The main issue was that we were not allowing None to be set for the items_per_page.
    
    The solution was to update the protocol files along with the handling of the value(starting in the dialog response, ending in the backend)
    
    Also important tests were added for the authentication vendors and authentication apps.
    
    Change-Id: I6f6b2916d8e91b109bf49f65f2801397b09b3ad3

--- mrs_plugin/tests/unit/helpers.py
+++ mrs_plugin/tests/unit/helpers.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022, Oracle and/or its affiliates.
+# Copyright (c) 2022, 2023, Oracle and/or its affiliates.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License, version 2.0,
@@ -20,6 +20,8 @@
 # 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 import pytest
+from ... import lib
+
 from ... schemas import add_schema, delete_schema
 from ... services import add_service, delete_service, get_service
 from ... db_objects import add_db_object, delete_db_object
@@ -58,3 +60,36 @@ class ServiceCT(object):
     def __exit__(self, type, value, traceback):
         assert delete_service(service_id=self._service_id) == True
 
+class AuthAppCT():
+    def __init__(self, session, **kwargs):
+        self._session = session
+        params = {
+            "service_id": kwargs.get("service_id"),
+            "auth_vendor_id": kwargs.get("auth_vendor_id"),
+            "name": kwargs.get("name"),
+            "description": kwargs.get("description"),
+            "url": kwargs.get("url"),
+            "url_direct_auth": kwargs.get("url_direct_auth"),
+            "access_token": kwargs.get("access_token"),
+            "app_id": kwargs.get("app_id"),
+            "limit_to_reg_users": kwargs.get("limit_to_reg_users"),
+            "default_role_id": kwargs.get("default_role_id"),
+        }
+        self._auth_app_id = lib.auth_apps.add_auth_app(session,
+            params["service_id"],
+            params["auth_vendor_id"],
+            params["name"],
+            params["description"],
+            params["url"],
+            params["url_direct_auth"],
+            params["access_token"],
+            params["app_id"],
+            params["limit_to_reg_users"],
+            params["default_role_id"]
+        )
+
+    def __enter__(self):
+        return self._auth_app_id
+
+    def __exit__(self, type, value, traceback):
+        lib.auth_apps.delete_auth_app(self._session, self._auth_app_id)
---
+++ mrs_plugin/tests/unit/lib/test_auth_apps.py
@@ -0,0 +1,94 @@
+# Copyright (c) 2023, Oracle and/or its affiliates.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2.0,
+# as published by the Free Software Foundation.
+#
+# This program is also distributed with certain software (including
+# but not limited to OpenSSL) that is licensed under separate terms, as
+# designated in a particular file or component or in included license
+# documentation.  The authors of MySQL hereby grant you an additional
+# permission to link the program and your derivative works with the
+# separately licensed software that they have included with MySQL.
+# This program is distributed in the hope that it will be useful,  but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+# the GNU General Public License, version 2.0, for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+import pytest
+from .... import lib
+from ...conftest import TableContents
+from ..helpers import AuthAppCT
+
+def test_get_auth_app(init_mrs, table_contents):
+    session = init_mrs["session"]
+
+    auth_apps_table: TableContents = table_contents("auth_app")
+
+    auth_app = lib.auth_apps.get_auth_app(session, init_mrs["auth_app_id"])
+
+    assert auth_app["auth_vendor"] == "MRS"
+
+    # auth_vendor is not part of the table, so remove to compare
+    del auth_app["auth_vendor"]
+
+    assert auth_apps_table.items == [auth_app]
+
+    new_auth_app_data = {
+        "service_id": init_mrs["service_id"],
+        "auth_vendor_id": lib.core.id_to_binary("0x30000000000000000000000000000000", "auth_vendor_id"),
+        "name": "New Auth App",
+        "description": "This is the new test auth app description",
+        "url": "http://someurl.com",
+        "url_direct_auth": "<some auth>",
+        "access_token": "<some token>",
+        "app_id": "<some app id>",
+        "enabled": True,
+        "limit_to_registered_users": False,
+        "default_role_id": init_mrs["roles"]["Full Access"]
+    }
+
+    with AuthAppCT(session, **new_auth_app_data) as auth_app_id:
+        assert auth_app_id
+        assert not auth_app_id == auth_app["id"]
+        assert auth_apps_table.count == auth_apps_table.snapshot.count + 1
+
+        new_auth_app = lib.auth_apps.get_auth_app(session, auth_app_id)
+
+        assert new_auth_app["id"] == auth_app_id
+
+        assert new_auth_app["auth_vendor"] == "MRS"
+
+        for key, value in new_auth_app_data.items():
+            assert new_auth_app[key] == value
+
+    assert auth_apps_table.same_as_snapshot
+
+
+def test_get_auth_apps(init_mrs, table_contents):
+    session = init_mrs["session"]
+
+    auth_apps_table: TableContents = table_contents("auth_app")
+
+    auth_apps = lib.auth_apps.get_auth_apps(session, init_mrs["service_id"])
+
+    assert len(auth_apps) == 1
+
+    new_auth_app_data = {
+        "service_id": init_mrs["service_id"],
+        "auth_vendor_id": lib.core.id_to_binary("0x30000000000000000000000000000000", "auth_vendor_id"),
+        "name": "New Auth App",
+    }
+
+    with AuthAppCT(session, **new_auth_app_data) as auth_app_id:
+        assert auth_app_id
+        assert auth_apps_table.count == auth_apps_table.snapshot.count + 1
+
+        auth_apps = lib.auth_apps.get_auth_apps(session, init_mrs["service_id"])
+
+        assert len(auth_apps) == 2
+
