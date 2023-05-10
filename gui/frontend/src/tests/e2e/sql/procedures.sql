c9d1ee5fea835b0e4d0870210fdf603922b2e4d8
Guilherme Saraiva <guilherme.saraiva@oracle.com>
2023-05-10 10:06:34 +0100

    Add missing SQL data to E2E
    This patch adds the required SQL data scripts to our repo
    
    Change-Id: Icbfcf206a3b093b76057169d71d18b688d6c6a37

---
+++ gui/frontend/src/tests/e2e/sql/procedures.sql
@@ -0,0 +1,83 @@
+use sakila;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `clearSchemas`()
+BEGIN
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE dbname VARCHAR(255);
+    DECLARE cur CURSOR FOR SELECT schema_name
+      FROM information_schema.schemata
+     WHERE schema_name LIKE 'testschema%' ESCAPE '\\'
+     ORDER BY schema_name;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+    OPEN cur;
+
+    read_loop: LOOP
+        FETCH cur INTO dbname;
+
+        IF done THEN
+          LEAVE read_loop;
+        END IF;
+
+        SET @query = CONCAT('DROP DATABASE ',dbname);
+        PREPARE stmt FROM @query;
+        EXECUTE stmt;
+    END LOOP;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `clearTables`()
+BEGIN
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE tablename VARCHAR(255);
+    DECLARE cur CURSOR FOR SELECT table_name FROM information_schema.tables 
+	where table_schema = database()
+	AND table_name like "testtable%" ESCAPE '\\';
+    
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+    OPEN cur;
+
+    read_loop: LOOP
+        FETCH cur INTO tablename;
+
+        IF done THEN
+          LEAVE read_loop;
+        END IF;
+
+        SET @query = CONCAT('DROP TABLE ', tablename);
+        PREPARE stmt FROM @query;
+        EXECUTE stmt;
+    END LOOP;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `clearViews`()
+BEGIN
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE viewname VARCHAR(255);
+    DECLARE cur CURSOR FOR SELECT table_name FROM information_schema.views 
+	WHERE table_schema = database()
+	AND table_name LIKE "%testview%" ESCAPE '\\';
+    
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+    OPEN cur;
+
+    read_loop: LOOP
+        FETCH cur INTO viewname;
+
+        IF done THEN
+          LEAVE read_loop;
+        END IF;
+
+        SET @query = CONCAT('DROP VIEW ', viewname);
+        PREPARE stmt FROM @query;
+        EXECUTE stmt;
+    END LOOP;
+END$$
+DELIMITER ;
+
