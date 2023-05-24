98d32e440b4a9e2b7c69b454f7c8877128ac8bec
Rene Ramirez <j.rene.ramirez@oracle.com>
2023-05-24 14:31:29 -0600

    Updating expectation to work with newer versions of websocket-client
    Change-Id: Ic5a9c908cd01c577cd5c5e55536e14441a5543f8

--- gui/backend/gui_plugin/debugger/scripts/unit/global/request_errors/no_token_single_user_mode.js
+++ gui/backend/gui_plugin/debugger/scripts/unit/global/request_errors/no_token_single_user_mode.js
@@ -1,5 +1,5 @@
 ws.connect();
 
 ws.validateLastResponse({
-    "exception": "Handshake status 400 Provided token is wrong."
+    "exception": ws.matchRegexp("Handshake status 400 Provided token is wrong.*")
 })
