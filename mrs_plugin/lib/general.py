1c85ee8eb511e992ab4609fbfa97c45a3b303cbc
Mike Zinner <mike.zinner@oracle.com>
2023-08-24 12:50:44 +0200

    Expose SRID of GEOMETRY columns in the MRS metadata schema
    Change-Id: I6feb04cc5fe37ee2b4e9d9a7ea588137a1f7ebd4

--- mrs_plugin/lib/general.py
+++ mrs_plugin/lib/general.py
@@ -24,7 +24,7 @@ from mrs_plugin import lib
 # Define plugin version
 VERSION = "1.12.1"
 
-DB_VERSION = [2, 1, 0]
+DB_VERSION = [2, 1, 1]
 REQUIRED_ROUTER_VERSION = [8, 0, 33]
 
 DB_VERSION_STR = '%d.%d.%d' % tuple(DB_VERSION)
