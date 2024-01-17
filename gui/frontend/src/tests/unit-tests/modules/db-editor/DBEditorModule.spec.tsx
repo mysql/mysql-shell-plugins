646a7600b5c0195cdf520cb74369e9f64cf3441f
Milosz Bodzek <milosz.bodzek@oracle.com>
2024-01-17 13:20:24 +0100

    Added unit tests for DBEditorModule
    Change-Id: I4931fbdb64f3609598ee0945b764d80079d3a3dd

--- gui/frontend/src/tests/unit-tests/modules/db-editor/DBEditorModule.spec.tsx
+++ gui/frontend/src/tests/unit-tests/modules/db-editor/DBEditorModule.spec.tsx
@@ -23,13 +23,24 @@
  * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+
 import icon from "../../../../assets/images/modules/module-sql.svg";
 
 import { createRef } from "preact";
 import { mount } from "enzyme";
 
 import { DBEditorModuleId } from "../../../../modules/ModuleInfo.js";
-import { DBEditorModule } from "../../../../modules/db-editor/DBEditorModule.js";
+import { IDBEditorTabInfo, DBEditorModule } from "../../../../modules/db-editor/DBEditorModule.js";
+import { appParameters } from "../../../../supplement/Requisitions.js";
+import { Button } from "../../../../components/ui/Button/Button.js";
+import { Divider } from "../../../../components/ui/Divider/Divider.js";
+import {
+    DBConnectionTab, IOpenEditorState, IDBConnectionTabPersistentState,
+} from "../../../../modules/db-editor/DBConnectionTab.js";
+import { EntityType } from "../../../../modules/db-editor/index.js";
+import { DBType, IConnectionDetails } from "../../../../supplement/ShellInterface/index.js";
+import { Dropdown } from "../../../../components/ui/Dropdown/Dropdown.js";
+import { nextProcessTick, nextRunLoop } from "../../test-helpers.js";
 
 describe("DBEditor module tests", (): void => {
 
@@ -51,4 +62,102 @@ describe("DBEditor module tests", (): void => {
         component.unmount();
     });
 
+    it("Test DBEditorModule get info", () => {
+        const component = mount<DBEditorModule>(<DBEditorModule />);
+        const info = DBEditorModule.info;
+        expect(info.id).toBe(DBEditorModuleId);
+        expect(info.caption).toBe("DB Editor");
+        expect(info.icon).toBeDefined();
+        component.unmount();
+    });
+
+    it("Test DBEditorModule embedded is true scenario", () => {
+        const originalEmbedded = appParameters.embedded;
+        appParameters.embedded = true;
+        const component = mount<DBEditorModule>(<DBEditorModule />);
+        expect(DBEditorModule.info).toStrictEqual({
+            id: DBEditorModuleId,
+            caption: "DB Editor",
+            icon,
+        });
+
+        const dropDownItemList = component.find(Dropdown.Item);
+        expect(dropDownItemList).toHaveLength(0);
+
+        component.unmount();
+        appParameters.embedded = originalEmbedded;
+    });
+
+    it("Test DBEditorModule selectedPage is 'connections' scenario", async () => {
+        const originalEmbedded = appParameters.embedded;
+        appParameters.embedded = true;
+        const component = mount<DBEditorModule>(<DBEditorModule />);
+        var toolbarItems = {
+            navigation: [],
+        };
+        component.setState({ selectedPage: "connections", toolbarItems });
+        await nextProcessTick();
+
+        const button = component.find(Button).at(0);
+        expect(button).toBeDefined();
+
+        const divider = component.find(Divider);
+        expect(divider).toBeDefined();
+
+        component.unmount();
+        appParameters.embedded = originalEmbedded;
+    });
+
+
+    it("Test DBEditorModule selectedPage is empty", async () => {
+        const originalEmbedded = appParameters.embedded;
+        appParameters.embedded = true;
+        const component = mount<DBEditorModule>(<DBEditorModule />);
+        var toolbarItems = {
+            navigation: [],
+        };
+        component.setState({ selectedPage: "", toolbarItems });
+        await nextProcessTick();
+
+        component.unmount();
+        appParameters.embedded = originalEmbedded;
+    });
+
+    it("Test DBEditorModule editorTabs are not empty scenario", () => {
+        const originalEmbedded = appParameters.embedded;
+        appParameters.embedded = true;
+
+        const connection1: IConnectionDetails = {
+            id: 1,
+            dbType: DBType.MySQL,
+            caption: "Mysql1",
+            description: "",
+            options: { type: "unknown" },
+            useSSH: false,
+            useMDS: false,
+        };
+
+        const connection2: IConnectionDetails = {
+            id: 2,
+            dbType: DBType.Sqlite,
+            caption: "Sqlite1",
+            description: "",
+            options: { type: "unknown" },
+            useSSH: false,
+            useMDS: false,
+        };
+
+        const editorTabs: IDBEditorTabInfo[] = [
+            { tabId: "tab1", caption: "Tab 1", details: connection1, suppressAbout: false },
+            { tabId: "tab2", caption: "Tab 2", details: connection2, suppressAbout: false },
+        ];
+
+        const component = mount<DBEditorModule>(<DBEditorModule />);
+
+        const instance = component.instance();
+        instance.setState({ editorTabs: editorTabs, connections: [connection1, connection2] });
+
+        component.unmount();
+        appParameters.embedded = originalEmbedded;
+    });
 });
