ec087ae596b70dade33ddd409eddd5fb5d2470df
Milosz Bodzek <milosz.bodzek@oracle.com>
2024-07-02 10:56:33 +0200

    Added more unit tests to DbEditorToolbar module
    Change-Id: I3416afab6941aa6641210869fed569e8c5c2eeaf

--- gui/frontend/src/tests/unit-tests/modules/db-editor/DBEditorToolbar.spec.tsx
+++ gui/frontend/src/tests/unit-tests/modules/db-editor/DBEditorToolbar.spec.tsx
@@ -26,12 +26,16 @@
 import { mount } from "enzyme";
 
 import { DBEditorToolbar } from "../../../../modules/db-editor/DBEditorToolbar.js";
+import { IOpenEditorState } from "../../../../modules/db-editor/DBConnectionTab.js";
+import { EntityType } from "../../../../modules/db-editor/index.js";
+import { CodeEditorMode } from "../../../../components/ui/CodeEditor/index.js";
+import { requisitions } from "../../../../supplement/Requisitions.js";
 
 describe("DBEditorToolbar tests", (): void => {
     it("Test DBEditorToolbar instantiation", () => {
         const component = mount<DBEditorToolbar>(
             <DBEditorToolbar
-                language={"pl"}
+                language={"ts"}
                 activeEditor={"DbEditor"}
                 heatWaveEnabled={false}
                 editors={[]}
@@ -40,7 +44,7 @@ describe("DBEditorToolbar tests", (): void => {
         );
 
         const props = component.props();
-        expect(props.language).toEqual("pl");
+        expect(props.language).toEqual("ts");
         expect(props.activeEditor).toEqual("DbEditor");
         expect(component).toMatchSnapshot();
         component.unmount();
@@ -49,7 +53,7 @@ describe("DBEditorToolbar tests", (): void => {
     it("Test DBEditorToolbar instantiation (HeatWave)", () => {
         const component = mount<DBEditorToolbar>(
             <DBEditorToolbar
-                language={"en"}
+                language={"js"}
                 activeEditor={"DbEditor"}
                 heatWaveEnabled={true}
                 editors={[]}
@@ -58,9 +62,128 @@ describe("DBEditorToolbar tests", (): void => {
         );
 
         const props = component.props();
-        expect(props.language).toEqual("en");
+        expect(props.language).toEqual("js");
         expect(props.activeEditor).toEqual("DbEditor");
         expect(component).toMatchSnapshot();
+        component.unmount();
+    });
+
+    it("Test DBEditorToolbar instantiation language=msg", () => {
+        const component = mount<DBEditorToolbar>(
+            <DBEditorToolbar
+                language={"msg"}
+                activeEditor={"DbEditor"}
+                heatWaveEnabled={false}
+                editors={[]}
+                toolbarItems={{ navigation: [], execution: [], editor: [], auxillary: [] }}
+            />,
+        );
+
+        const props = component.props();
+        expect(props.language).toEqual("msg");
+        expect(props.activeEditor).toEqual("DbEditor");
+        expect(component).toMatchSnapshot();
+        component.unmount();
+    });
+
+    it("Test DBEditorToolbar requisitions editorCaretMoved", async () => {
+        const component = mount<DBEditorToolbar>(
+            <DBEditorToolbar
+                language={"msg"}
+                activeEditor={"DbEditor"}
+                heatWaveEnabled={false}
+                editors={[]}
+                toolbarItems={{ navigation: [], execution: [], editor: [], auxillary: [] }}
+            />,
+        );
+
+        await requisitions.execute("editorCaretMoved", {
+            lineNumber: 1,
+            column: 1,
+        });
+
+
+        component.unmount();
+    });
+
+    it("Test DBEditorToolbar requisitions editorContextStateChanged", async () => {
+        const component = mount<DBEditorToolbar>(
+            <DBEditorToolbar
+                language={"msg"}
+                activeEditor={"DbEditor"}
+                heatWaveEnabled={false}
+                editors={[]}
+                toolbarItems={{ navigation: [], execution: [], editor: [], auxillary: [] }}
+            />,
+        );
+
+        const id = component.state().currentContext?.id;
+        await requisitions.execute("editorContextStateChanged", id);
+
+
+        component.unmount();
+    });
+
+    it("Test DBEditorToolbar requisitions editorToggleStopExecutionOnError", async () => {
+        const component = mount<DBEditorToolbar>(
+            <DBEditorToolbar
+                language={"msg"}
+                activeEditor={"DbEditor"}
+                heatWaveEnabled={false}
+                editors={[]}
+                toolbarItems={{ navigation: [], execution: [], editor: [], auxillary: [] }}
+            />,
+        );
+
+        await requisitions.execute("editorToggleStopExecutionOnError", true);
+
+        component.unmount();
+    });
+
+    it("Test DBEditorToolbar requisitions settingsChanged", async () => {
+        const component = mount<DBEditorToolbar>(
+            <DBEditorToolbar
+                language={"msg"}
+                activeEditor={"DbEditor"}
+                heatWaveEnabled={false}
+                editors={[]}
+                toolbarItems={{ navigation: [], execution: [], editor: [], auxillary: [] }}
+            />,
+        );
+
+        await requisitions.execute("settingsChanged", undefined);
+
+        component.unmount();
+    });
+
+    it("Test DBEditorToolbar call componentDidUpdate", () => {
+        const component = mount<DBEditorToolbar>(
+            <DBEditorToolbar
+                language={"en"}
+                activeEditor={"DbEditor1"}
+                heatWaveEnabled={true}
+                editors={[]}
+                toolbarItems={{ navigation: [], execution: [], editor: [], auxillary: [] }}
+            />,
+        );
+
+        const testEditorState: IOpenEditorState = {
+            id: "DbEditor1",
+            type: EntityType.Notebook,
+            caption: "DbEditor1",
+            dbDataId: 123,
+            currentVersion: 1,
+        };
+
+        const props = component.props();
+        expect(props.language).toEqual("en");
+        expect(props.activeEditor).toEqual("DbEditor1");
+
+        props.editors = [testEditorState];
+        component.setProps(props);
+        props.activeEditor = "DbEditor2";
+        component.instance().componentDidUpdate(props);
+
         component.unmount();
     });
 
