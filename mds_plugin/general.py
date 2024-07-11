b9821c27fc61d8de7306af39816e7c0107b2744c
Rene Ramirez <j.rene.ramirez@oracle.com>
2024-07-11 15:59:51 -0600

    Updating version to 1.15.2
    Change-Id: I83d05c3db3f3a6ec7f4c2b92c8f7a1bbfec7bf26

--- mds_plugin/general.py
+++ mds_plugin/general.py
@@ -27,7 +27,7 @@ from mysqlsh.plugin_manager import plugin_function
 from mds_plugin import core, configuration
 
 # Define plugin version
-VERSION = "1.15.1"
+VERSION = "1.15.2"
 
 
 @plugin_function('mds.info')
