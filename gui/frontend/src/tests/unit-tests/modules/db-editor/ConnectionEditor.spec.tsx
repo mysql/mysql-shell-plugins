b0de714234fda5276504182a23e22c6ba0926fd1
Milosz Bodzek <milosz.bodzek@oracle.com>
2024-07-30 11:33:20 +0200

    Added more unit tests for ConnectionEditor module
    Change-Id: I05f713f3418ef8a3c70206a7c6515a0d70ad8e1b

--- gui/frontend/src/tests/unit-tests/modules/db-editor/ConnectionEditor.spec.tsx
+++ gui/frontend/src/tests/unit-tests/modules/db-editor/ConnectionEditor.spec.tsx
@@ -27,6 +27,7 @@ import { mount } from "enzyme";
 
 import { screen, waitFor } from "@testing-library/preact";
 import { IMySQLConnectionOptions, MySQLConnectionScheme } from "../../../../communication/MySQL.js";
+import { IDialogValidations, IDialogValues } from "../../../../components/Dialogs/ValueEditDialog.js";
 import { ConnectionEditor } from "../../../../modules/db-editor/ConnectionEditor.js";
 import { ShellInterface } from "../../../../supplement/ShellInterface/ShellInterface.js";
 import { ShellInterfaceSqlEditor } from "../../../../supplement/ShellInterface/ShellInterfaceSqlEditor.js";
@@ -36,6 +37,17 @@ import { MySQLShellLauncher } from "../../../../utilities/MySQLShellLauncher.js"
 import {
     DialogHelper, changeInputValue, getDbCredentials, nextRunLoop, setupShellForTests,
 } from "../../test-helpers.js";
+
+class TestConnectionEditor extends ConnectionEditor {
+    public testValidateConnectionValues = (closing: boolean, values: IDialogValues,
+        data?: IDictionary): IDialogValidations => {
+        return this.validateConnectionValues(closing, values, data);
+    };
+
+    public testGenerateEditorConfig = (details?: IConnectionDetails): IDialogValues => {
+        return this.generateEditorConfig(details);
+    };
+}
 
 describe("ConnectionEditor tests", (): void => {
     let launcher: MySQLShellLauncher;
@@ -126,6 +138,346 @@ describe("ConnectionEditor tests", (): void => {
         await nextRunLoop();
         portals = document.getElementsByClassName("portal");
         expect(portals.length).toBe(0);
+
+        component.unmount();
+    });
+
+    it("Test ConnectionEditor updating connection", async () => {
+        const component = mount<ConnectionEditor>(
+            <ConnectionEditor
+                connections={[testMySQLConnection]}
+                onAddConnection={() => { }}
+                onUpdateConnection={() => { }} />,
+        );
+
+        let portals = document.getElementsByClassName("portal");
+        expect(portals.length).toBe(0);
+
+        await component.instance().show("MySQL", false, testMySQLConnection);
+
+        await waitFor(() => {
+            expect(screen.getByText("Host Name or IP Address")).toBeDefined();
+        });
+
+        portals = document.getElementsByClassName("portal");
+        expect(portals.length).toBe(1);
+
+        const timeout = screen.getByText("Host Name or IP Address");
+        changeInputValue(timeout, "127.0.0.1");
+
+        const protocol = screen.getByText("Protocol");
+        changeInputValue(protocol, MySQLConnectionScheme.MySQL);
+
+        const port = screen.getByText("Port");
+        changeInputValue(port, "3308");
+
+        const user = screen.getByText("User Name");
+        changeInputValue(user, "user1");
+
+        const clearPassword = screen.getByText("Clear Password");
+        clearPassword.click();
+
+        const defaultSchema = screen.getByText("Default Schema");
+        changeInputValue(defaultSchema, "schema1");
+
+        const useSSH = screen.getByText("Connect using SSH Tunnel");
+        useSSH.click();
+
+        const useBastion = screen.getByText("Connect using OCI Bastion service");
+        useBastion.click();
+
+        await nextRunLoop();
+
+        dialogHelper.verifyErrors();
+        await dialogHelper.clickOk();
+
+        await nextRunLoop();
+        portals = document.getElementsByClassName("portal");
+        expect(portals.length).toBe(0);
+
+        component.unmount();
+    });
+
+    it("Test ConnectionEditor validateConnectionValues function when databaseType is not selected", () => {
+        const component = mount<TestConnectionEditor>(<TestConnectionEditor
+            connections={[testMySQLConnection]}
+            onAddConnection={() => { }}
+            onUpdateConnection={() => { }} />,
+        );
+        const instance = component.instance();
+
+        const values: IDialogValues = {
+            sections: new Map([
+                ["general", {
+                    values: {
+                        databaseType: { value: "" },
+                    },
+                }],
+                ["information", {
+                    values: {
+                        caption: { value: "Connection 1" },
+                    },
+                }],
+                ["mysqlDetails", {
+                    values: {},
+                }],
+                ["sqliteDetails", {
+                    values: {},
+                }],
+                ["mysqlAdvanced", {
+                    values: {},
+                }],
+            ]),
+        };
+
+        const result = instance.testValidateConnectionValues(false, values);
+
+        expect(result.requiredContexts).toEqual([]);
+        expect(result.messages).toEqual({ databaseType: "Select one of the database types for your connection" });
+
+        component.unmount();
+    });
+
+    it("Test ConnectionEditor validateConnectionValues function when caption is empty", () => {
+        const component = mount<TestConnectionEditor>(<TestConnectionEditor
+            connections={[testMySQLConnection]}
+            onAddConnection={() => { }}
+            onUpdateConnection={() => { }} />,
+        );
+        const instance = component.instance();
+
+        const values: IDialogValues = {
+            sections: new Map([
+                ["general", {
+                    values: {
+                        databaseType: { value: "MySQL" },
+                    },
+                }],
+                ["information", {
+                    values: {
+                        caption: { value: "" },
+                    },
+                }],
+                ["mysqlDetails", {
+                    values: {
+                        port: { value: 3306 },
+                        hostName: { value: "localhost" },
+                        userName: { value: "user1" },
+                    },
+                }],
+                ["sqliteDetails", {
+                    values: {},
+                }],
+                ["mysqlAdvanced", {
+                    values: {
+                        timeout: { value: 10 },
+                    },
+                }],
+            ]),
+        };
+
+        const result = instance.testValidateConnectionValues(true, values);
+
+        expect(result.requiredContexts).toEqual([]);
+        expect(result.messages).toEqual({ caption: "The caption cannot be empty" });
+
+        component.unmount();
+    });
+
+    it("Test ConnectionEditor validateConnectionValues function when dbFilePath is not specified for Sqlite", () => {
+        const component = mount<TestConnectionEditor>(<TestConnectionEditor
+            connections={[testMySQLConnection]}
+            onAddConnection={() => { }}
+            onUpdateConnection={() => { }} />,
+        );
+        const instance = component.instance();
+
+        const values: IDialogValues = {
+            sections: new Map([
+                ["general", {
+                    values: {
+                        databaseType: { value: "Sqlite" },
+                    },
+                }],
+                ["information", {
+                    values: {
+                        caption: { value: "Connection 1" },
+                    },
+                }],
+                ["mysqlDetails", {
+                    values: {},
+                }],
+                ["sqliteDetails", {
+                    values: {
+                        dbFilePath: { value: "" },
+                    },
+                }],
+                ["mysqlAdvanced", {
+                    values: {},
+                }],
+            ]),
+        };
+
+        const result = instance.testValidateConnectionValues(true, values);
+
+        expect(result.requiredContexts).toEqual([]);
+        expect(result.messages).toEqual({ dbFilePath: "Specify the path to an existing Sqlite DB file" });
+
+        component.unmount();
+    });
+
+    it("Test ConnectionEditor validateConnectionValues function when hostName is not specified for MySQL", () => {
+        const component = mount<TestConnectionEditor>(<TestConnectionEditor
+            connections={[testMySQLConnection]}
+            onAddConnection={() => { }}
+            onUpdateConnection={() => { }} />,
+        );
+        const instance = component.instance();
+
+        const values: IDialogValues = {
+            sections: new Map([
+                ["general", {
+                    values: {
+                        databaseType: { value: "MySQL" },
+                    },
+                }],
+                ["information", {
+                    values: {
+                        caption: { value: "Connection 1" },
+                    },
+                }],
+                ["mysqlDetails", {
+                    values: {
+                        hostName: { value: "" },
+                        userName: { value: "user1" },
+                        port: { value: 3306 },
+                    },
+                }],
+                ["sqliteDetails", {
+                    values: {},
+                }],
+                ["mysqlAdvanced", {
+                    values: {
+                        timeout: { value: 10 },
+                    },
+                }],
+            ]),
+        };
+
+        const result = instance.testValidateConnectionValues(true, values);
+
+        expect(result.requiredContexts).toEqual([]);
+        expect(result.messages).toEqual({ hostName: "Specify a valid host name or IP address" });
+
+        component.unmount();
+    });
+
+    it("Test ConnectionEditor validateConnectionValues function when userName is not specified for MySQL", () => {
+        const component = mount<TestConnectionEditor>(<TestConnectionEditor
+            connections={[testMySQLConnection]}
+            onAddConnection={() => { }}
+            onUpdateConnection={() => { }} />,
+        );
+        const instance = component.instance();
+
+        const values: IDialogValues = {
+            sections: new Map([
+                ["general", {
+                    values: {
+                        databaseType: { value: "MySQL" },
+                    },
+                }],
+                ["information", {
+                    values: {
+                        caption: { value: "Connection 1" },
+                    },
+                }],
+                ["mysqlDetails", {
+                    values: {
+                        hostName: { value: "localhost" },
+                        userName: { value: "" },
+                        port: { value: 3306 },
+                    },
+                }],
+                ["sqliteDetails", {
+                    values: {},
+                }],
+                ["mysqlAdvanced", {
+                    values: {
+                        timeout: { value: 10 },
+                    },
+                }],
+            ]),
+        };
+
+        const result = instance.testValidateConnectionValues(true, values);
+
+        expect(result.requiredContexts).toEqual([]);
+        expect(result.messages).toEqual({ userName: "The user name must not be empty" });
+
+        component.unmount();
+    });
+
+    it("Test ConnectionEditor validateConnectionValues function when port is not a valid integer for MySQL", () => {
+        const component = mount<TestConnectionEditor>(<TestConnectionEditor
+            connections={[testMySQLConnection]}
+            onAddConnection={() => { }}
+            onUpdateConnection={() => { }} />,
+        );
+        const instance = component.instance();
+
+        const values: IDialogValues = {
+            sections: new Map([
+                ["general", {
+                    values: {
+                        databaseType: { value: "MySQL" },
+                    },
+                }],
+                ["information", {
+                    values: {
+                        caption: { value: "Connection 1" },
+                    },
+                }],
+                ["mysqlDetails", {
+                    values: {
+                        hostName: { value: "localhost" },
+                        userName: { value: "user1" },
+                        port: { value: -1 },
+                    },
+                }],
+                ["sqliteDetails", {
+                    values: {},
+                }],
+                ["mysqlAdvanced", {
+                    values: {
+                        timeout: { value: 10 },
+                    },
+                }],
+            ]),
+        };
+
+        const result = instance.testValidateConnectionValues(true, values);
+
+        expect(result.requiredContexts).toEqual([]);
+        expect(result.messages).toEqual({ timeout: "The port must be a valid integer >= 0" });
+
+        component.unmount();
+    });
+
+    it("Test ConnectionEditor generateEditorConfig function", () => {
+        const component = mount<TestConnectionEditor>(<TestConnectionEditor
+            connections={[testMySQLConnection]}
+            onAddConnection={() => { }}
+            onUpdateConnection={() => { }} />,
+        );
+        const instance = component.instance();
+
+        const result = instance.testGenerateEditorConfig(testMySQLConnection);
+
+        expect(result.sections.get("information")!.values.caption.value).toEqual(testMySQLConnection.caption);
+        expect(result.sections.get("information")!.values.description.value).toEqual(testMySQLConnection.description);
+        expect(result.sections.get("mysqlDetails")!.values.hostName.value).toEqual(testMySQLConnection.options.host);
+        expect(result.sections.get("mysqlDetails")!.values.userName.value).toEqual(testMySQLConnection.options.user);
 
         component.unmount();
     });
