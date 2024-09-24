deb2d331e89da6b0b66a179f3059ec93be1e2e6f
Alfredo Kojima <alfredo.kengi.kojima@oracle.com>
2024-09-24 12:06:15 -0700

    Fixed parser execution context
    MrsDdlExecutor had localhost/service stored in current_service while
    current_service_host was always None. This fixes it so that current_service
    contains just the service path (/service) and current_service_host contains
    the hostname (localhost)
    
    Change-Id: I85a4bfc528a81e7b8bb07fad0b5e75c25c91f59a

--- mrs_plugin/lib/MrsDdlExecutor.py
+++ mrs_plugin/lib/MrsDdlExecutor.py
@@ -154,7 +154,8 @@ class MrsDdlExecutor(MrsDdlExecutorInterface):
             service = lib.services.get_current_service(self.session)
             if service is not None:
                 self.state_data['current_service_id'] = service.get("id")
-                self.state_data['current_service'] = service.get("host_ctx")
+                self.state_data['current_service'] = service.get("url_context_root")
+                self.state_data["current_service_host"] = service.get("url_host_name")
 
     @property
     def current_service_id(self):
@@ -184,6 +185,7 @@ class MrsDdlExecutor(MrsDdlExecutorInterface):
 
     @property
     def current_service_host(self):
+        self.lookup_current_service()
         return self.state_data.get('current_service_host')
 
     @current_service_host.setter
