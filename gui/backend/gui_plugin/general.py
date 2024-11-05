dcbc6383f5d0159e680c43b80a142a28158ed6a4
Rene Ramirez <j.rene.ramirez@oracle.com>
2024-11-05 14:32:29 -0600

    Updated version to 1.17.0
    Updated CHANGELOG
    
    Change-Id: I592814b3a6c11dac09e85d7cf1430eb7b85f352d

--- gui/backend/gui_plugin/general.py
+++ gui/backend/gui_plugin/general.py
@@ -24,7 +24,7 @@
 from mysqlsh.plugin_manager import plugin_function
 
 # Define plugin version
-VERSION = "1.16.5"
+VERSION = "1.17.0"
 
 @plugin_function('gui.info', shell=True, cli=True, web=True)
 def info():
--- mds_plugin/general.py
+++ mds_plugin/general.py
@@ -27,7 +27,7 @@ from mysqlsh.plugin_manager import plugin_function
 from mds_plugin import core, configuration
 
 # Define plugin version
-VERSION = "1.16.5"
+VERSION = "1.17.0"
 
 
 @plugin_function('mds.info')
