21e541e00fddd260d0fbb981ef1daaa0ddf478bf
Mike Zinner <mike.zinner@oracle.com>
2024-11-26 19:13:19 +0100

    Add Version String to Docs
    Change-Id: Iae42f6f2bab0d96d97dc7e26c33507b7b2a7da6a

--- mrs_plugin/scripts/generate_html_docs.sh
+++ mrs_plugin/scripts/generate_html_docs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2022, 2023, Oracle and/or its affiliates.
+# Copyright (c) 2022, 2024, Oracle and/or its affiliates.
 
 if [ $# -eq 0 ]
 then
@@ -8,11 +8,13 @@ else
     ROOTPATH=$1
 fi
 
+version="$(cat $ROOTPATH/../VERSION | grep -o "[^=]\+\$")"
+
 cd $ROOTPATH/docs
 
 # Generate the docs
 echo "Generating HTML docs ..."
-pandoc index.md -f markdown -t html -s -o $ROOTPATH/docs/index.html --template=$ROOTPATH/docs/templates/mysql_docs.html --toc --toc-depth=2 --metadata title="MRS Developer's Guide" --variable=template_css:style/style.css --filter pandoc-include --number-sections
-pandoc restApi.md -f markdown -t html -s -o $ROOTPATH/docs/restApi.html --template=$ROOTPATH/docs/templates/mysql_docs.html --toc --toc-depth=2 --metadata title="MRS Core REST APIs" --variable=template_css:style/style.css --filter pandoc-include --number-sections
-pandoc sdk.md -f markdown -t html -s -o $ROOTPATH/docs/sdk.html --template=$ROOTPATH/docs/templates/mysql_docs.html --toc --toc-depth=2 --metadata title="MRS SDK Reference" --variable=template_css:style/style.css --filter pandoc-include --number-sections
-pandoc sql.md -f markdown -t html -s -o $ROOTPATH/docs/sql.html --template=$ROOTPATH/docs/templates/mysql_docs.html --toc --toc-depth=2 --metadata title="MRS SQL Reference" --variable=template_css:style/style.css --filter pandoc-include --number-sections
+pandoc index.md -f markdown -t html -s -o $ROOTPATH/docs/index.html --template=$ROOTPATH/docs/templates/mysql_docs.html --toc --toc-depth=2 --metadata title="MRS Developer's Guide" --variable=template_css:style/style.css --filter pandoc-include --number-sections -V version="$version"
+pandoc restApi.md -f markdown -t html -s -o $ROOTPATH/docs/restApi.html --template=$ROOTPATH/docs/templates/mysql_docs.html --toc --toc-depth=2 --metadata title="MRS Core REST APIs" --variable=template_css:style/style.css --filter pandoc-include --number-sections -V version="$version"
+pandoc sdk.md -f markdown -t html -s -o $ROOTPATH/docs/sdk.html --template=$ROOTPATH/docs/templates/mysql_docs.html --toc --toc-depth=2 --metadata title="MRS SDK Reference" --variable=template_css:style/style.css --filter pandoc-include --number-sections -V version="$version"
+pandoc sql.md -f markdown -t html -s -o $ROOTPATH/docs/sql.html --template=$ROOTPATH/docs/templates/mysql_docs.html --toc --toc-depth=2 --metadata title="MRS SQL Reference" --variable=template_css:style/style.css --filter pandoc-include --number-sections -V version="$version"
