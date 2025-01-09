0d098a86e9737c73fcffa91004bc1d8d5397c989
Alexander Hitrov <alexander.hitrov@oracle.com>
2025-01-09 14:06:16 +0200

    Remove redirect after extension settings change
    Change-Id: I9dc7e16326ece8d0f916eb97cb6f7a38e5e484a7

--- gui/extension/src/ExtensionHost.ts
+++ gui/extension/src/ExtensionHost.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, 2024, Oracle and/or its affiliates.
+ * Copyright (c) 2021, 2025, Oracle and/or its affiliates.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2.0,
@@ -255,7 +255,7 @@ export class ExtensionHost {
 
         await Promise.all(this.providers.map((provider) => {
             if (sender === undefined || provider !== sender) {
-                return provider.runCommand(requestType, parameter);
+                return provider.runCommand(requestType, parameter, "", false); // Skipping reveal.
             }
 
             return Promise.resolve();
--- gui/frontend/src/supplement/RequisitionTypes.ts
+++ gui/frontend/src/supplement/RequisitionTypes.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, Oracle and/or its affiliates.
+ * Copyright (c) 2024, 2025, Oracle and/or its affiliates.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2.0,
@@ -295,7 +295,7 @@ export interface IWebviewProvider {
 
     close(): void;
     runCommand<K extends keyof IRequestTypeMap>(requestType: K, parameter: IRequisitionCallbackValues<K>,
-        caption: string, settingName: string): Promise<boolean>;
+        caption: string, reveal: boolean): Promise<boolean>;
 }
 
 /**
