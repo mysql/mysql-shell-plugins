e7ba9cba8136bc96ddfd6fdf84b0243ef3886f0a
Rene Ramirez <j.rene.ramirez@oracle.com>
2025-04-10 18:51:12 -0600

    Updating version to 1.19.7
    Change-Id: I7b98a280806ac235aa1ecadd575c15f5995a3238

--- mrs_plugin/lib/general.py
+++ mrs_plugin/lib/general.py
@@ -25,7 +25,7 @@ from mrs_plugin import lib
 import msm_plugin.lib.management as schema_management
 
 # Define plugin version
-VERSION = "1.19.6"
+VERSION = "1.19.7"
 
 DB_VERSION = [4, 0, 3]
 REQUIRED_ROUTER_VERSION = [8, 1, 0]
