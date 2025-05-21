4a2578e2596a184002aa29eddd7a3dda6ffd1a09
Rene Ramirez <j.rene.ramirez@oracle.com>
2025-05-20 21:22:05 -0600

    Packaging fixes:
    - The -v option to identify the existence of the SHELL_PLUGINS_LOCATION
      for custom plugin locations is not supported in the bash version of
      macos, replaced it by the -z option.
    - The fix_vbs.sh script was copying the vbs folder into out, but if
      called several times (i.e. from build_packages.sh) it ended nesting
      the vbs folder inside of the already copied vbs folder. Updated
      the logic to delete the existing vbs folder.
    
    Change-Id: I29cb842b7ff38b8b83a5e6b5e48c916a697584b3

--- gui/extension/scripts/copy_vbs.sh
+++ gui/extension/scripts/copy_vbs.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-# Copyright (c) 2024, Oracle and/or its affiliates.
+# Copyright (c) 2024, 2025, Oracle and/or its affiliates.
 
 if [ ! -d "node_modules/regedit/vbs" ]; then
     echo "ERROR: The vbs in regedit is missing"
     exit 1
+fi
+
+# Ensure the vbs foldes does not exist, otherwise it will
+# end up nesting it
+if [ -d "out/vbs" ]; then
+   rm -rf out/vbs;
 fi
 
 echo "Copy vbs files for regedit operations"
